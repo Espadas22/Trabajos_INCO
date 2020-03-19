3838301b737ff8aa176c24a33cf9ba698efab323
Anthony <espadasrodriguezaj@gmail.com>
2020-03-19 00:40:29 -0600

    Version final de la practica 8 de FFC

--- Practicas FFC/d05-p08-anthony-espadas.py
+++ Practicas FFC/d05-p08-anthony-espadas.py
@@ -7,11 +7,14 @@ print("espadas rodriguez anthony jonathan");
 print("mariscal cervantes diego maximiliano\n");
 
 def e1(): #Calculo para el radio de una esfera
-    radio = float(input("Dame el radio de una esfera\n"));
+    radio = float(input("Introduce el radio de una esfera\n"));
+
     while radio <= 0:
         print("El radio debe ser mayor que cero");
         radio = float(input("introduce un valor valido\n"));
-    area = 4 * 3.1416 * radio;
+
+    area = 4 * 3.14159265 * radio**2;
+
     print("El area de una esfera de radio %.2f es: %.2f " % (radio, area));
 
 def e2(): #Cacular volumen de una cantidad n de cubos
@@ -28,9 +31,10 @@ def e2(): #Cacular volumen de una cantidad n de cubos
             print("El valor debe ser positivo para calcular o 0 para salir");
             arista = float(input("Ingresa un valor valido\n"));
             
-        contador += 1;
-        volumen = arista**3;
-        print("\nEl volumen del cubo", contador-1," con arista de %.2f es de: %.2f" % (arista, volumen));
+        if arista > 0:
+            contador += 1;
+            volumen = arista**3;
+            print("\nEl volumen del cubo", contador-1," con arista de %.2f es de: %.2f" % (arista, volumen));
 
 def e3(): #Generador de tablas de multiplicar, desde 0 a n
     tabla = int(input("De cual numero quieres conocer la tabla?\n"));
@@ -52,22 +56,18 @@ def e4(): #Inversor de signo de una cantidad n de numeros
         print("La cantidad de numeros debe ser al menos 1");
         repeticiones = int(input("Ingresa un valor valido\n"));
     
-    contador = 0;
-    conversion = "";
+    contador = 1;
 
-    while repeticiones > contador:
-        numero = int(input("A que numero le vamos a cambiar el signo?\n"));
-        contrario = str(numero*-1);
-        conversion += contrario;
-        conversion += ", "
+    while contador <= repeticiones:
+        print("\nPara el" ,contador, "numero");
+        ingresado = float(input("Introduce la cantidad\n"));
+        inverso = ingresado *(-1);
+        print("\nIngresaste" ,ingresado, ", el numero con signo contrario es " ,inverso);
         contador += 1;
-    
-    print("Los numeros invertidos son", conversion);
-
 
 def e5(): #Promedia las calificaciones de un numero n de alumnos y menciona la mayor
-    repeticiones = int(input("Cuantas calificaciones vamos a registrar\n"));
-    contador = 0;
+    repeticiones = int(input("Agrega la cantidad de alumnos en el grupo\n"));
+    contador = 1;
     suma = 0;
     calificacion_maxima = 0;
 
@@ -75,8 +75,8 @@ def e5(): #Promedia las calificaciones de un numero n de alumnos y menciona la m
         print("Se debe registrar al menos una calificacion");
         repeticiones = int(input("Introduce un valor valido\n"));
 
-    while repeticiones != contador:
-        print("Dame la calificacion del alumno", contador+1);
+    while repeticiones >= contador:
+        print("Dame la calificacion del alumno", contador);
         calificacion = float(input());
 
         while (calificacion > 100) | (calificacion < 0):
@@ -85,17 +85,18 @@ def e5(): #Promedia las calificaciones de un numero n de alumnos y menciona la m
 
         if calificacion > calificacion_maxima:
             calificacion_maxima = calificacion;
-            mejor_alumno = contador+1;
+            mejor_alumno = contador;
 
         suma += calificacion;
         contador += 1;
 
     promedio = suma/repeticiones;
-    print("\nEl promedio fue", promedio);
+
+    print("\nEl promedio del grupo con %.0f alumnos fue: %.2f" % (repeticiones, promedio));
     print("\nLa calificacion mas alta fue", calificacion_maxima,"del alumno", mejor_alumno);
 
 def e6(): #Determina los colores de una cantidad n de autos
-    repeticiones = int(input("Cuantos carros vamos a evaluar?\n"));
+    repeticiones = int(input("Indica la cantidad de autos que entran a la ciudad de Mexico\n"));
 
     while repeticiones < 1:
         print("El numero de carros debe ser al menos 1");
@@ -109,32 +110,45 @@ def e6(): #Determina los colores de una cantidad n de autos
     azules = 0;
 
     while contador <= repeticiones:
-        print("Cual es el ultimo digito de las placas del carro", contador);
-        digito = int(input());
+        print("\nIngresa la placa del auto", contador);
+        placa = int(input());
 
-        while (digito > 9) | (digito < 0):
-            print("Los valores deben estar entre 0 y 9");
-            digito = int(input("Ingresa un valor dentro del rango\n"));
+        while (placa < 99999) | (placa > 999999):
+            print("Valor de placa invalido");
+            placa = int(input("Las placas deben constar de 6 digitos\n"));
+
+        digito = placa % 10;
 
         if (digito == 1) | (digito == 2):
+            print("El automovil " ,contador, ", con placa" ,placa, ", tiene calcomania amarilla");
             amarillos += 1;
         elif (digito == 3) | (digito == 4):
+            print("El automovil " ,contador, ", con placa" ,placa, ", tiene calcomania rosa");
             rosas += 1;
         elif (digito == 5) | (digito == 6):
+            print("El automovil " ,contador, ", con placa" ,placa, ", tiene calcomania roja");
             rojos += 1;
         elif (digito == 7) | (digito == 8):
+            print("El automovil " ,contador, ", con placa" ,placa, ", tiene calcomania verde");
             verdes += 1;
         else:
+            print("El automovil " ,contador, ", con placa" ,placa, ", tiene calcomania azul");
             azules += 1;
             
         contador += 1;
         
-    print("\nDe un total de", repeticiones, "autos");
-    print(amarillos, "son amarillos");
-    print(rosas, "son rosas");
-    print(rojos, "son rojos");
-    print(verdes, "son verdes");
-    print(azules, "son azules");
+    print("\nDel total de", repeticiones, "autos que ingresaron a la CDMX:");
+
+    if amarillos > 0:
+        print(amarillos, "son amarillos");
+    if rosas > 0:
+        print(rosas, "son rosas");
+    if rojos > 0:
+        print(rojos, "son rojos");
+    if verdes > 0:
+        print(verdes, "son verdes");
+    if azules > 0:
+        print(azules, "son azules");
     
             
 def menu():
@@ -145,7 +159,7 @@ def menu():
         print("(2) Para calcular el volumen de varios cubos");
         print("(3) Para calcular tablas de multiplicar");
         print("(4) Para invertir el signo de varias cifras");
-        print("(5) Para calcular promedio de un grupode alumnos");
+        print("(5) Para calcular promedio de un grupo de alumnos");
         print("(6) Para calcular el numero de autos de cada color");
         print("(0) Para salir\n");
 
@@ -155,7 +169,9 @@ def menu():
             print("El valor debe estar entre 0 y 6");
             eleccion = int(input("Ingresa un valor dentro del rango\n"));
 
-        if eleccion == 1:
+        if eleccion == 0:
+            print("\nGracias por emplear nuestro sistema. Hasta pronto.");
+        elif eleccion == 1:
             e1();
         elif eleccion == 2:
             e2();
--- Practicas FFC/d05-p09-anthony-espadas.py
+++ Practicas FFC/d05-p09-anthony-espadas.py
@@ -6,8 +6,38 @@ print("practica  09");
 print("espadas rodriguez anthony jonathan");
 print("mariscal cervantes diego maximiliano\n");
 
+def validarValor():
+    try:
+        valor = int(input("Introduce un valor valido\n"));
+        if valor < 0:
+            print("El valor no puede ser menor que cero");
+            valor = validarValor();
+    except:
+        print("Los valores deben ser numeros enteros");
+        valor = validarValor();
+    return valor;
+
+def validarEleccion():
+    eleccion = str(input("Ingresa una respuesta valida\n"));
+    if (eleccion != "si") & (eleccion != "no"):
+        print("Las respuestas solo pueden ser 'si' o 'no'");
+        eleccion = validarEleccion();
+    return eleccion;
+
+def validarCalificacion():
+    try:
+        calificacion = float(input("Ingresa un valor dentro del rango\n"));
+        if (calificacion < 0) | (calificacion > 100):
+            print("Las calificaciones deben estar entre 0 y 100");
+            calificacion = validarCalificacion();
+    except:
+        print("La calificacion debe ser un numero entre 0 y 100");
+        calificacion = validarCalificacion();
+    return calificacion;
+
 def e01(): #Calculo de descuentos en un teatro segun edad
-    tope = 2;
+    contador = 0;
+    tope = 1;
     boleto = 80;
     categoria_1 = categoria_2 = categoria_3 = categoria_4 = categoria_5 = 0;
 
@@ -15,42 +45,94 @@ def e01(): #Calculo de descuentos en un teatro segun edad
         try:
             edad = int(input("Ingresa la edad del cliente:\n"));
 
+            if edad < 0:
+                print("La edad no puede ser menor que cero");
+                edad = validarValor();
         except:
-            print("La edad debe ser un entero positivo");
+            print("La edad solo puede ser un numero entero");
+            edad = validarValor();
 
-        if edad < 0:
-            print("La edad debe ser un entero positivo");
-        elif edad < 5:
+        if edad < 5:
             print("Lo sentimos, no ingresan menores de 5 años");
         elif edad < 15:
             precio = boleto*.65;
             print("Categoria 1, tienes 35%% de descuento, paga solo %.2f" % (precio));
             categoria_1 += boleto - precio;
+            contador += 1;
         elif edad < 20:
             precio = boleto*.75
             print("Categoria 2, tienes 25%% de descuento, paga solo %.2f" % (precio));
             categoria_2 += boleto - precio;
+            contador += 1;
         elif edad < 46:
             precio = boleto*.90;
             print("Categoria 3, tienes 10%% de descuento, paga solo %.2f" % (precio));
             categoria_3 += boleto - precio;
+            contador += 1;
         elif edad < 65:
             precio = boleto*.75
             print("Categoria 4, tienes 25%% de descuento, paga solo %.2f" % (precio));
             categoria_4 += boleto - precio;
+            contador += 1;
         elif edad < 125:
             precio = boleto*.65;
             print("Categoria 5, tienes 35%% de descuento, paga solo %.2f" % (precio));
             categoria_5 += boleto - precio;
+            contador += 1;
         else:
             print("Nadie ha vivido tanto, verifique sus datos")
 
         eleccion = str(input("Quieres ingresar otro usuario? (si/no)\n"));
 
+        if (eleccion != "si") & (eleccion != "no"):
+            print("Las respuestas solo pueden ser 'si' o 'no'")
+            eleccion = validarEleccion();
+
         if eleccion == "si":
-            tope +=1;
+            tope += 1;
 
-def imprime():
-    e01();
+def e02(): #Calculador de masa de aire en n vehiculos
+    dsds
 
-imprime();
+
+def menu():
+    eleccion = 11;
+    while eleccion != 0:
+        print("\nElije un ejercicio para ejecutar:");
+        print("(1) Para determinar las perdidas de ingreso en un teatro segun la edad de sus clientes");
+        print("(2) Para calcular el promedio de masa de aire de los neumaticos de n vehiculos");
+        print("(3) Para calcular el precio del kilo de huevo segun sea la calidad de la gallina");
+        print("(4) Para conocer la inclinacion de los diputados ante en TLC");
+        print("(5) Realizar una suma de 2 en 2 (usando hexadecimal)");
+        print("(6) Comprar la califiacion de n cantidad de alumnos");
+        print("(0) Para salir\n");
+
+        try:
+            eleccion = int(input());
+        except:
+            print("La eleccion solo puede ser un numero entero");
+
+        while (eleccion < 0) | (eleccion > 6):
+            print("El valor debe estar entre 0 y 6");
+            try:
+                eleccion = int(input("Ingresa un valor dentro del rango\n"));
+            except:
+                print("La eleccion solo puede ser un numero entero");
+
+        if eleccion == 1:
+            e01();
+        elif eleccion == 2:
+            e02();
+        elif eleccion == 3:
+            e03();
+        elif eleccion == 4:
+            e04();
+        elif eleccion == 5:
+            e05();
+        elif eleccion == 6:
+            e06();
+
+def imprimir():
+    menu();
+
+imprimir();
