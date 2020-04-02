6ba72a9af2803db6ad450ed8b6477f28a38dbfb4
Anthony <espadasrodriguezaj@gmail.com>
2020-04-02 00:52:12 -0600

    Figuras respetan el espacio delimitado

--- Practicas FFC/d05-p10-anthony-espadas.py
+++ Practicas FFC/d05-p10-anthony-espadas.py
@@ -5,7 +5,7 @@
 def e1():
     import sys; 
     from colorama import init, Fore, Style, Cursor;
-    init(autoreset=True);
+    init(autoreset=True); 
 
     def imprimirDatos():
         print(Fore.MAGENTA + "practica 10");
@@ -17,7 +17,7 @@ def e1():
         print(Fore.CYAN + "\b (3)Circulo ", end=""); 
         print(Fore.CYAN + "\b (0)Salir ", end="");
         print(Fore.WHITE + "\b ||", end=" ");
-        print(Fore.CYAN + "Usa las flechas para mover la figura seleccionada\n"); 
+        print(Fore.CYAN + "Usa las direcciones W A S D para mover la figura\n"); 
     
     def dibujarArea(): #Dibuja el cuadrado que delimita las figuras
         for i in range(7, 30):
@@ -25,15 +25,19 @@ def e1():
                 if i == 7 or i == 29 or j == 1 or j == 119: #Valida estar en la primer o ultima linea
                     print(Fore.WHITE + Style.BRIGHT + Cursor.POS(j,i) + "*"); 
                     
-    #Funciones que se encargan de dibujar el asterisco del color corresponiente
-    def asteriscoVerde(i, j):      
-        print(Fore.GREEN + Style.BRIGHT + Cursor.POS(j, i) + "*"); 
+    #Funcion que se encarga de dibujar el asterisco del color corresponiente
+    def dibujarAsterisco(linea, columna, color): 
+        linea = ((linea + 7) % 22) + 7; 
+        columna = (columna % 119); 
 
-    def asteriscoRojo(i, j): 
-        print(Fore.RED + Style.DIM + Cursor.POS(j, i) + ".");   
-
-    def asteriscoAmarillo(i, j): 
-        print(Fore.YELLOW + Style.DIM + Cursor.POS(j, i) + "."); 
+        if color == 1:
+            print(Fore.GREEN + Style.BRIGHT + Cursor.POS(columna, linea) + "*"); 
+        elif color == 2:
+            print(Fore.YELLOW + Style.DIM + Cursor.POS(columna, linea) + ".");  
+        elif color == 3: 
+            print(Fore.RED + Style.DIM + Cursor.POS(columna, linea) + "."); 
+        elif color == 4:
+            print(Fore.WHITE + Style.DIM + Cursor.POS(columna, linea) + "."); 
 
     def moverTriangulo(columna, linea, color): #Se encarga de dibujar al triangulo en consola
         distancia = 0; #Genera la separacion entre los bordes
@@ -42,22 +46,22 @@ def e1():
             for j in range(columna - distancia, columna + distancia): #Abre distancia de los lados segun se acerca a la base
                 if i == linea or i == linea + 10: #Valida estar en la primer o ultima linea 
                     if color == 1:
-                        asteriscoVerde(i, j); 
+                        dibujarAsterisco(i, j, 1); 
                     elif color == 2:
-                        asteriscoAmarillo(i, j); 
+                        dibujarAsterisco(i, j, 2); 
                     elif color == 3:
-                        asteriscoRojo(i, j); 
+                        dibujarAsterisco(i, j, 3); 
                     else:
-                        print(Fore.WHITE + Style.DIM + Cursor.POS(j, i) + "."); 
+                        dibujarAsterisco(i, j, 4); 
                 elif j == columna - distancia or j == columna + distancia -1: #Valida estar en los extremos
                     if color == 1:
-                        asteriscoVerde(i, j); 
+                        dibujarAsterisco(i, j, 1); 
                     elif color == 2:
-                        asteriscoAmarillo(i, j); 
+                        dibujarAsterisco(i, j, 2); 
                     elif color == 3:
-                        asteriscoRojo(i, j); 
+                        dibujarAsterisco(i, j, 3); 
                     else:
-                        print(Fore.WHITE + Style.DIM + Cursor.POS(j, i) + "."); 
+                        dibujarAsterisco(i, j, 4); 
             
             distancia += 1; 
 
@@ -66,78 +70,78 @@ def e1():
             for j in range(columna, columna + 21): #Genera lado de 20 columnas
                 if i == linea or i == linea + 10: #Revisa que nos encontremos en los bordes
                     if color == 1:
-                        asteriscoVerde(i, j); 
+                        dibujarAsterisco(i, j, 1); 
                     elif color == 2:
-                        asteriscoAmarillo(i, j); 
+                        dibujarAsterisco(i, j, 2); 
                     elif color == 3:
-                        asteriscoRojo(i, j); 
+                        dibujarAsterisco(i, j, 3); 
                 elif j == columna or j == columna + 20: #E imprime el asterisco
                     if color == 1:
-                        asteriscoVerde(i, j); 
+                        dibujarAsterisco(i, j, 1); 
                     elif color == 2:
-                        asteriscoAmarillo(i, j); 
+                        dibujarAsterisco(i, j, 2); 
                     elif color == 3:
-                        asteriscoRojo(i, j); 
+                        dibujarAsterisco(i, j, 3); 
 
     def moverCirculo(columna, linea, color):
         for i in range(linea, linea + 11):
             for j in range(columna, columna + 21):
                 if (i == linea or i == linea + 10) and (j > columna + 2 and j < columna + 17): #Verificamos estar en borde horizontal
                     if color == 1:
-                        asteriscoVerde(i, j); 
+                        dibujarAsterisco(i, j, 1); 
                     elif color == 2:
-                        asteriscoAmarillo(i, j); 
+                        dibujarAsterisco(i, j, 2); 
                     elif color == 3:
-                        asteriscoRojo(i, j); 
+                        dibujarAsterisco(i, j, 3); 
                 elif (j == columna or j == columna + 19) and (i > linea + 1 and i < linea + 9): #Verifcamos estar en un borde vertical
                     if color == 1:
-                        asteriscoVerde(i, j); 
+                        dibujarAsterisco(i, j, 1); 
                     elif color == 2:
-                        asteriscoAmarillo(i, j); 
+                        dibujarAsterisco(i, j, 2); 
                     elif color == 3:
-                        asteriscoRojo(i, j); 
+                        dibujarAsterisco(i, j, 3); 
                 elif (j == columna + 1 and i == linea + 1) or (j == columna + 1 and i == linea + 9) or (j == columna + 18 and i == linea + 1) or (j == columna + 18 and i == linea + 9): #Verificamos estar en una esquina
                     if color == 1:
-                        asteriscoVerde(i, j); 
+                        dibujarAsterisco(i, j, 1); 
                     elif color == 2:
-                        asteriscoAmarillo(i, j); 
+                        dibujarAsterisco(i, j, 2); 
                     elif color == 3:
-                        asteriscoRojo(i, j); 
+                        dibujarAsterisco(i, j, 3); 
     
     #Funciones que dibujan la pantalla segun la figura seleccionada
     def pintarPantallaCuadrado(cuadrado_x, cuadrado_y, color_cuadrado, triangulo_x, triangulo_y, color_triangulo, circulo_x, circulo_y, color_circulo):
         print('\x1b[2J', end = ""); 
         imprimirDatos(); 
-        dibujarArea();  
         moverTriangulo(triangulo_x, triangulo_y, color_triangulo); 
         moverCirculo(circulo_x, circulo_y, color_circulo); 
         moverCuadrado(cuadrado_x, cuadrado_y, color_cuadrado); 
+        dibujarArea(); 
 
     def pintarPantallaTriangulo(cuadrado_x, cuadrado_y, color_cuadrado, triangulo_x, triangulo_y, color_triangulo, circulo_x, circulo_y, color_circulo):
         print('\x1b[2J', end = ""); 
         imprimirDatos(); 
-        dibujarArea(); 
         moverCuadrado(cuadrado_x, cuadrado_y, color_cuadrado);  
         moverCirculo(circulo_x, circulo_y, color_circulo); 
-        moverTriangulo(triangulo_x, triangulo_y, color_triangulo);  
+        moverTriangulo(triangulo_x, triangulo_y, color_triangulo);
+        dibujarArea();   
 
     def pintarPantallaCirculo(cuadrado_x, cuadrado_y, color_cuadrado, triangulo_x, triangulo_y, color_triangulo, circulo_x, circulo_y, color_circulo):
         print('\x1b[2J', end = ""); 
         imprimirDatos(); 
-        dibujarArea(); 
         moverCuadrado(cuadrado_x, cuadrado_y, color_cuadrado);  
         moverTriangulo(triangulo_x, triangulo_y, color_triangulo);
         moverCirculo(circulo_x, circulo_y, color_circulo); 
+        dibujarArea(); 
 
     #Posiciones en X y Y de las figuras
     triangulo_x = 59; 
-    triangulo_y = 12; 
+    triangulo_y = 21; 
     
     cuadrado_x = 10; 
-    cuadrado_y = 12; 
+    cuadrado_y = 21; 
 
     circulo_x = 85; 
-    circulo_y = 12; 
+    circulo_y = 21; 
 
     pintarPantallaCuadrado(cuadrado_x, cuadrado_y, 2, triangulo_x, triangulo_y, 4, circulo_x, circulo_y, 3); 
 
